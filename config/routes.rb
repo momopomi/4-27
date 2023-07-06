29309bc2843480d60196ffd3d040952f085533f8
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-07-06 03:27:29 +0000

    [edit]4-27

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
+  get "books/index" => "books#index", :as => :user_root
   resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:show, :index, :edit]
 end
